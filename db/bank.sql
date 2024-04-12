d32d4780f9cbb53801a925d1f3a12d8d900e59cb
Jacka10086 <496806255@qq.com>
2024-04-12 11:31:07 +0100

    1

--- db/bank.sql
+++ db/bank.sql
@@ -1,1 +1,36 @@
+/*CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+); 
 
+CREATE TABLE IF NOT EXISTS abstracts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    content TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS abstracts_comments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    abstract_id INT NOT NULL,
+    comment TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (abstract_id) REFERENCES abstracts(id)
+);
+
+CREATE TABLE IF NOT EXISTS introductory_materials (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    content TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS introductory_comments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    intro_material_id INT NOT NULL,
+    comment TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (intro_material_id) REFERENCES introductory_materials(id)
+);
+
+
+ */
